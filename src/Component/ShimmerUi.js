ad2801016aa52cdcfea57d6c41527fb18d9937fd
Abhishek Rajendra Shimpi <83231585+abhi-shimpi@users.noreply.github.com>
2024-08-10 16:01:06 +0530

    Update ShimmerUi.js

--- src/Component/ShimmerUi.js
+++ src/Component/ShimmerUi.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ShimmerUi() {
     return (
-        <div className='flex justify-center flex-wrap p-14 gap-4'>
+        <div className='flex justify-center flex-wrap p-14 gap-4 pt-[120px]'>
             <div className="shimmer-card">
                 <span className="loader-element"></span>
             </div>
@@ -29,6 +29,9 @@ function ShimmerUi() {
             </div>
             <div className="shimmer-card">
                 <span className="loader-element"></span>
+            </div>
+            <div className="shimmer-card">
+                    <span className="loader-element"></span>
             </div>
         </div>
     )
